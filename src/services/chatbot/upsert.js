9cbd0bc3e9970a0ab82de53c1acaba98e7dd5774
OneDev1231 <dev.top.guru@gmail.com>
2024-06-23 19:20:16 +0300

    0623 upload feature updated

--- src/services/chatbot/upsert.js
+++ src/services/chatbot/upsert.js
@@ -1,24 +1,23 @@
 import axios from 'axios';
 
 export const existing_chatbot_upsert_file = async (chatbotName, uploadedFile) => {
-    return 200
-    // const formData = new FormData();
-    // formData.append("file", uploadedFile);
-    // formData.append("chatbotName", chatbotName);
+    const formData = new FormData();
+    formData.append("file", uploadedFile);
+    formData.append("chatbotName", chatbotName);
 
-    // try {
-    //     const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/upsert_file`, formData, {
-    //         headers: {
-    //             'Content-Type': 'multipart/form-data'
-    //         },
-    //         withCredentials: true
-    //     });
-    //     console.log('File uploaded successfully:', response);
-    //     return response.status;
-    // } catch (error) {
-    //     console.error('Error uploading response:', error.response);
-    //     return error.response.status;
-    // }
+    try {
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/upsert_file`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+            withCredentials: true
+        });
+        console.log('File uploaded successfully:', response);
+        return response.status;
+    } catch (error) {
+        console.error('Error uploading response:', error.response);
+        return error.response.status;
+    }
 }
 
 export const existing_chatbot_upsert_text = async (chatbotName, text) => {
