58ad04597c99d612acbb710fd106ec9c8301a803
OneDev1231 <dev.top.guru@gmail.com>
2024-06-27 17:01:11 +0300

    0627 solve href issue

--- src/components/home/pricing.jsx
+++ src/components/home/pricing.jsx
@@ -89,6 +89,7 @@ export default function Pricing({ dictionary }) {
           </div>
           <a
             href="https://app.formbricks.com/s/clxxagx0c04538moleczafoc2"
+            target="_blank"
             aria-describedby="standard-tier"
             className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
